ce82e4280f7d1fccbc54e9e78817bc16abe2b7ef
Jian Weng <jian.weng@ucla.edu>
2018-06-13 11:15:21 -0700

    simulate annealing alog tuner!

--- tvm-auto/conv-gpu.py
+++ tvm-auto/conv-gpu.py
@@ -2,6 +2,7 @@
 
 import topi, tvm, autotvm, time, numpy, logging, operator
 from autotvm import MeasureResult, MeasureErrorNo
+from sa import SATuner
 
 def conv(iw, ih, fw, fh, fi, fo, batch, dtype):
     img = tvm.placeholder((batch, fi, iw, ih), dtype=dtype, name='img')
@@ -21,38 +22,41 @@ def conv(iw, ih, fw, fh, fi, fo, batch, dtype):
 
     cfg.add_flop(iw * ih * fw * fh * fi * fo * batch * 2)
 
-    nr_axes  = [cfg.axis(i) for i in conv.op.axis]
-    nr_splits = [cfg.split('tile_' + i.name, i, policy='all', num_split=2) for i in nr_axes]
-    ord_axes = cfg.reorder('reorder', reduce(list.__add__, nr_splits), policy='interleave', spatial=nr_splits, reduce=[])
-    cfg.annotate('binds', ord_axes[:sum([len(axis) - 1 for axis in nr_splits])], policy='bind_gpu_virtual')
+    #nr_axes  = [cfg.axis(i) for i in conv.op.axis]
+    #nr_splits = [cfg.split('tile_' + i.name, i, policy='all', num_split=2) for i in nr_axes]
+    #ord_axes = cfg.reorder('reorder', reduce(list.__add__, nr_splits), policy='interleave', spatial=nr_splits, reduce=[])
+    #cfg.annotate('binds', ord_axes[:sum([len(axis) - 1 for axis in nr_splits])], policy='bind_gpu_virtual')
 
-    red_axes  = [cfg.axis(i) for i in conv.op.reduce_axis]
-    red_splits= [cfg.split('tile_red_' + i.name, i, policy='all', num_split=2) for i in red_axes]
+    #red_axes  = [cfg.axis(i) for i in conv.op.reduce_axis]
+    #red_splits= [cfg.split('tile_red_' + i.name, i, policy='all', num_split=2) for i in red_axes]
 
-    nr_splits = [cfg['tile_' + i.var.name].apply(sch, conv, i) for i in sch[conv].op.axis]
-    #red_splits= [cfg['tile_red_' + i.var.name].apply(sch, conv, i) for i in sch[conv].op.reduce_axis]
-    order     = cfg['reorder'].apply(sch, conv, reduce(list.__add__, nr_splits))
-    #print(list(map(len, nr_splits)))
-    binds = cfg['binds'].apply(sch, conv, order[:sum([len(axis) - 1 for axis in nr_splits])])
+    #nr_splits = [cfg['tile_' + i.var.name].apply(sch, conv, i) for i in sch[conv].op.axis]
+    ##red_splits= [cfg['tile_red_' + i.var.name].apply(sch, conv, i) for i in sch[conv].op.reduce_axis]
+    #order     = cfg['reorder'].apply(sch, conv, reduce(list.__add__, nr_splits))
+    ##print(list(map(len, nr_splits)))
+    #binds = cfg['binds'].apply(sch, conv, order[:sum([len(axis) - 1 for axis in nr_splits])])
 
     #print tvm.lower(sch, [img, fil, conv], simple_mode=True)
+    autotvm.cuda.cuda_general_schedule(cfg, sch, conv)
 
     return sch, [img, fil, conv]
 
-#logging.basicConfig(level=logging.INFO, filename='random-gpu.log')
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, filename='sa-gpu.log')
+#logging.basicConfig(level=logging.INFO)
 tsk = autotvm.task.Task(conv, [226, 226, 3, 3, 64, 64, 1, 'float32'], {})
-tgt = tvm.target.create('cuda')
+tgt = tvm.target.create('cuda -model=p4000')
 tsk.init_space(tgt, None)
 print(tsk.config_space)
 
-#tuner = autotvm.tuner.GridSearchTuner(tsk)
-tuner = autotvm.tuner.RandomTuner(tsk)
+#tuner = autotvm.tuner.RandomTuner(tsk)
+#tuner = autotvm.tuner.GATuner(tsk, pop_size=128, elite_num=4)
+#tuner = autotvm.tuner.XGBTuner(tsk, tgt, 16, 4)
+tuner = SATuner(tsk, 16, 960)
 
 tuner.add_callback(autotvm.callback.SingleBestRecorder())
 
 fmeature = autotvm.fleet.get_measure_batch(
-    autotvm.fleet.create('titanx', timeout=10.0),
+    autotvm.fleet.create('titanx', timeout=20.0),
     tgt, 'llvm', repeat=5,
     retry_failures=None,
     replay_db=None,
---
+++ tvm-auto/sa.py
@@ -0,0 +1,116 @@
+import autotvm
+import random
+import numpy as np
+import logging
+from autotvm.tuner.tuner import Tuner
+
+class SATuner(Tuner):
+    def __init__(self, task, pool, total, shrink=0.9, T=None, target=None):
+        super(SATuner, self).__init__(task)
+
+        self.space = task.config_space
+        self.pool_size = pool
+        self.total = total
+        self.shrink = shrink
+
+        vals = self.space.space_map.values()
+
+        self.dims = [len(i) for i in vals] + [1]
+        self.n_dims = len(vals)
+        if T is None:
+            self.T = np.sqrt(sum(i * i for i in self.dims)) / pool
+        else:
+            self.T = T
+        self.N = reduce(int.__mul__, self.dims)
+
+        self.visited = set([])
+
+        self.pool = []
+        self.exec_pool = []
+        for i in range(pool):
+            temp = random.randint(0, self.N - 1)
+            while temp in self.visited:
+                temp = random.randint(0, self.N - 1)
+            self.pool.append((temp, 0))
+            self.exec_pool.append(temp)
+            self.visited.add(temp)
+
+            vec = self._i_to_vec(temp)
+            #print temp
+            #print vec
+            #print self._vec_to_i(vec)
+
+        self.cur = 0
+        self.better = False
+
+
+    def next_batch(self, batch_size):
+        res = []
+        for i in range(self.cur, min(self.pool_size, self.cur + batch_size)):
+            res.append(self.space.get(self.exec_pool[i]))
+            self.cur += 1
+        return res
+
+    def has_next(self):
+        return len(self.visited) - (self.pool_size - self.cur) < self.total
+
+    def update(self, inputs, results):
+        n = len(inputs)
+        assert n == len(results)
+        for i in range(n):
+            inp = inputs[i]
+            res = results[i]
+            sol, old_score = self.pool[i]
+            if res.error_no == 0:
+                new_score = inp.task.flop / np.mean(res.runs) / 2.5e12
+            else:
+                new_score = 0.0
+            if new_score > old_score:
+                logging.log(logging.INFO, '%f is better than %f' % (new_score, old_score))
+                self.pool[i] = (self.exec_pool[i], new_score)
+                self.better = True
+            elif random.random() < np.exp(-(old_score - new_score) / self.T):
+                per = np.exp(-(old_score - new_score) / self.T)
+                logging.log(logging.INFO, '%.2f%% accept a bad score %f (%f)' % (per * 100.0, new_score, old_score))
+                self.pool[i] = (self.exec_pool[i], new_score)
+
+        if self.cur >= self.pool_size:
+            if self.better:
+                self.T *= self.shrink
+                logging.log(logging.INFO, 'Current Temperature: %f' % self.T)
+                self.better = False
+
+            self.cur = 0
+            for i in range(self.pool_size):
+                sol, _ = self.pool[i]
+
+                def _explore(sol):
+                    vec = self._i_to_vec(sol)
+                    T = self.T
+                    for j in range(self.n_dims - 1):
+                        angle = (random.random() * np.pi * 2)
+                        vec[j] = (vec[j] + int(self.T * np.cos(angle))) % self.dims[j]
+                    T *= np.sin(angle)
+                    vec[-1] = (vec[-1] + int(T)) % self.dims[-1]
+                    return self._vec_to_i(vec)
+
+                temp = _explore(sol)
+                while temp in self.visited:
+                    temp = _explore(temp)
+                self.exec_pool[i] = temp
+            
+
+    def _i_to_vec(self, num):
+        res = []
+        for i in range(self.n_dims, 0, -1):
+            res.append(num % self.dims[i])
+            num /= self.dims[i]
+        res.reverse()
+        return res
+
+    def _vec_to_i(self, vec):
+        res = 0
+        for i in range(1, self.n_dims):
+            res = res * self.dims[i] + vec[i-1]
+        return res
+
